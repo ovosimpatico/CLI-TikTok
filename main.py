172ae8285def6dc6fe4a5f04b5191b2d8d02a8ca
nanometer5088 <code.deleo@simplelogin.com>
2023-01-13 21:40:57 -0300

    Downloader is here! * Downloader feature added * Error handling

--- main.py
+++ main.py
@@ -2,23 +2,28 @@ from src.functions import listas
 
 from src.streaming import playbackrandom, playback
 
+from src.downloader import downloadtiktoks
+
 import configparser
 configParser = configparser.RawConfigParser()
 configFilePath = r'config.cfg'
 configParser.read(configFilePath)
 
 random = str(configParser.get('DEFAULT', 'random'))
-
+downloader = str(configParser.get('DEFAULT', 'downloader'))
 
 urls = listas()[0]
 datas = listas()[1]
 
 try:
-    if random == "True":
-        playbackrandom(urls, datas)
-    elif random == "False":
-        playback(urls, datas)
-    else:
-        print("Something went wrong with your configuration")
+    if downloader == 'True':
+        downloadtiktoks(urls, datas)
+    if downloader == 'False':
+        if random == "True":
+            playbackrandom(urls, datas)
+        if random == "False":
+            playback(urls, datas)
+        else:
+            print("Something went wrong with your configuration")
 except KeyboardInterrupt:
     print("KeyboardInterrupt was detected.\nGoodbye!")
--- src/downloader.py
+++ src/downloader.py
@@ -1,1 +1,43 @@
-# Soon
+from src.functions import detect_dead_link
+from src.functions import url_redirection
+
+from yt_dlp import YoutubeDL
+
+import os
+
+def downloader(url):
+    ydl_opts = {
+    'ignoreerrors': True,
+    'format': 'bestvideo*+bestaudio/best',
+    'outtmpl': os.getcwd()+'/video/by-creator/%(creator)s/%(id)s.%(ext)s'
+    }
+    YoutubeDL(ydl_opts).download(url)
+    
+def downloadtiktoks(urls, datas):
+    index = -1
+    errorcount = []
+    a = input(f"""
+        **WARNING**
+        This action will download up to {len(urls)} tiktoks in CLI-TIkTok/video/
+        Ensure you have enough free space before proceeding!
+        \nPress ENTER to proceed...
+        """)
+    if a != '':
+        print('Operation canceled.')
+        return
+    while True:
+        os.system("cls || clear")
+        try:
+            randomvideo = index = index + 1
+            url = url_redirection(urls[randomvideo])
+        
+            if detect_dead_link(url) == True:
+                print(f'\nVideo Liked - {datas[randomvideo]}\n')
+                downloader(url)
+            else:
+                errorcount.append(urls)
+        except IndexError:
+            print("The tiktoks were downloaded")
+            if len(errorcount) != 0:
+                print(f'\n{len(errorcount)} video(s) failed to download.\nThe video(s) were likely banned or removed from the platform.')
+                break
