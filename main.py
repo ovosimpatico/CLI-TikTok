cb59a11cfa4954bbe9ad77699ac0e45d4e94871f
nanometer5088 <code.deleo@simplelogin.com>
2023-01-28 00:20:18 -0300

    Initial testing + proper comments

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
+# Introduction and pre-test
+from src.init import init
+import sys
+a = init()
+if a == -1:
+    print("Dependencies installed successfully.\nOpen the program again\n")
+    sys.exit()
+
+# Import libraries and required functions after sucessful pre-test
 from src.functions import listas
 from src.streaming import playbackrandom, playback
 from src.downloader import downloadtiktoks
 from src.byuser import streamuser, getLinks
-import sys, os, subprocess
+import os, subprocess
 
-
+# Needlessly big code to simply prompt the user which action they want to do
 try:
-    question = int(input("""Welcome to CLI TikTok, an open-source TikTok viewer!
-        
-        Do you want to download or watch tiktoks?
+    question = int(input("""Do you want to download or watch tiktoks?
         
         (1) Download
         (2) Watch
         """))
     os.system("cls || clear")
-    ## DOWNLOAD
+    
+    
+    ## Download
     if question == 1:
         downloadquestion = int(input("""Do you want to download your liked videos or a creator?
         
@@ -22,10 +31,14 @@ try:
         (2) Creator
         """))
         os.system("cls || clear")
+        
+        ## Download liked videos
         if downloadquestion == 1:
             urls = listas()[0]
             downloadtiktoks(urls)
             sys.exit()
+            
+        ## Download creator
         if downloadquestion == 2:
             print('Due to specific limitations of the current data method, downloading by creator will only get the latest 30 videos.')
             print('This limitation is being actively researched, any contributions will be welcome.')
@@ -34,7 +47,7 @@ try:
             downloadtiktoks(links)
             sys.exit()
         
-    ## STREAM
+    ## Stream
     if question == 2:
         
         watchquestion = int(input("""Do you want to watch your liked videos or a creator?
@@ -43,25 +56,32 @@ try:
         (2) Creator
         """))
         os.system("cls || clear")
+        
+        ## Stream liked videos
         if watchquestion == 1:
             
-            #Liked videos random?
+            
             randomquestion = int(input("""Do you want to watch the tiktoks in randomized order?
             (1) Yes
             (2) No                                       
         """))
             os.system("cls || clear")
+            
+            ## Stream liked videos randomized
             if randomquestion == 1:
                 urls = listas()[0]
                 datas = listas()[1]
                 playbackrandom(urls, datas)
                 sys.exit()
+                
+            ## Stream liked videos in descending order
             if randomquestion == 2:
                 urls = listas()[0]
                 datas = listas()[1]
                 playback(urls, datas)
                 sys.exit()
         
+        ## Stream creator
         if watchquestion == 2:
             print('Due to specific limitations of the current data method, watching by creator will only get the latest 30 videos.')
             print('This limitation is being actively researched, any contributions will be welcome.')
@@ -69,13 +89,22 @@ try:
             streamuser(username)
             sys.exit()
             
+    # Error handling for invalid number (3, 4, 6, 133)
     print("The option you chose isn't valid.")
+    
+    # Error handling for invalid input (ENTER, 't', '5ga')
 except ValueError:
     print("The option you chose isn't valid.")
+    
+    # Error handling for missing Likes.txt file
 except FileNotFoundError:
     print("The 'Likes.txt' file was not found. Make sure it is in the program folder and try again.")
+    
+    # Error handling for MPV media player or MPV not found in PATH
 except subprocess.CalledProcessError:
     os.system("cls || clear")
     print("Mpv media player was not found on your system path. Make sure it's installed and try again.")
+    
+    # Error handling for exiting the code with CTRL + C
 except KeyboardInterrupt:
-    print("\tKeyboardInterrupt was detected - Goodbye!")
+    print("\n\tKeyboardInterrupt was detected - Goodbye!")
---
+++ src/init.py
@@ -0,0 +1,49 @@
+from os import system
+import sys
+def init():
+    #Intro for the user
+    system("cls || clear")
+    input("Welcome to CLI TikTok, an open-source TikTok archiver and viewer!\nPress ENTER to proceed")
+
+    # Detect and install libraries - If they aren't installed,
+    # the user is prompted to make the auto-installation.
+    try:
+        import yt_dlp
+        import requests
+        import atoma
+        system("cls || clear")
+    except ModuleNotFoundError:
+        system("cls || clear")
+        input("""
+        The program detected dependencies are not installed
+          Press ENTER to install the necessary libraries
+          
+     (You will need to open the program again afterwards)
+              """)
+        system("pip install -r requirements.txt --user")
+        system("cls || clear")
+        return -1
+
+    # Search for updates, and prompts the user in case they're found.
+    # If the user does not have internet access, warns him the software won't work properly and quit.
+    try:
+        import requests
+        data = requests.get("https://raw.githubusercontent.com/nanometer5088/CLI-TikTok/main/VERSION")
+        version = open('VERSION', 'r', encoding='utf=8')
+        if version.readline().rstrip() > (data.text):
+            system("cls || clear")
+            input("""
+                      There's a new version available!
+            Updates bring performance and feature improvements!
+                    
+                      Download the new version here: 
+      https://github.com/nanometer5088/CLI-TikTok/archive/refs/heads/main.zip
+                        
+                        Press ENTER to proceed
+                """)
+        version.close()
+    except requests.exceptions.ConnectionError:
+        print("CLI-TikTok detected your device isn't connected to the internet")
+        print("This software requires a reliable and uncensored internet connection to properly work")
+        print("Please try again with an internet connection")
+        sys.exit()
